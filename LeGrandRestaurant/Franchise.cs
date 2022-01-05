e7b07518311e4014f1352d6376203bb87a883392
Lucas <lucas.jenvrain@gmail.com>
2022-01-05 09:01:44 +0100

    test

---
+++ LeGrandRestaurant/Franchise.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace LeGrandRestaurant
+{
+    public class Franchise
+    {
+        private readonly Menu _menu;
+
+        public Franchise(Restaurant restaurant)
+        {
+            _menu = new Menu();
+            restaurant.ImposerMenu(_menu);
+        }
+
+        public void FixerPrix(Plat plat, decimal nouveauPrix)
+            => _menu.FixerPrix(plat, nouveauPrix);
+    }
+}
---
+++ LeGrandRestaurant/Table.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LeGrandRestaurant
+{
+    public class Table
+    {
+        public bool EstLibre { get; private set; } = true;
+
+        public void InstallerClient()
+        {
+            if (!EstLibre) throw new InvalidOperationException();
+            EstLibre = false;
+        }
+
+        public void Libérer()
+        {
+            EstLibre = true;
+        }
+    }
+}
