f8cb654d553b0f6e96c63d7e32d6ea4ce5d8bdc0
Loukade <65962183+Loukade@users.noreply.github.com>
2022-02-24 19:54:36 +0100

    Add script bash

---
+++ runIntegration.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+git clone https://github.com/Loukade/ProjetTestUnitaire.git
+if dotnet build ProjetTestUnitaire/LeGrandRestaurant
+then
+	if dotnet test ProjetTestUnitaire/LeGrandRestaurant
+	then 
+		mkdir integration
+		cp -r ProjetTestUnitaire/LeGrandRestaurant integration
+	fi
+fi
+$SHELL
